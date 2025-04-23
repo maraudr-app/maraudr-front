186e8579a0b4f61337084dea036c50842353500d
HubertZodjihoue <joycetreavis@gmail.com>
2025-04-23 23:51:48 +0200

    test: config ci for real test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
   },
+  css: {
+    postcss: './postcss.config.js'
+  },
+
 })
