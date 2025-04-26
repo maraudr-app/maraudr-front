7727cc0f71e200f82ceedb23b3219661bbd0a46d
HubertZodjihoue <joycetreavis@gmail.com>
2025-04-26 22:52:34 +0200

    fix i18n traduction for landing page

---
+++ src/types/i18next.d.ts
@@ -0,0 +1,15 @@
+import 'i18next';
+
+// Déclarer un module pour augmenter les types d'i18next
+declare module 'i18next' {
+  // Étendre l'interface des ressources
+  interface CustomTypeOptions {
+    // Liste des namespaces supportés
+    defaultNS: 'common';
+    resources: {
+      common: typeof import('../../public/locales/fr/common.json');
+      dashboard: typeof import('../../public/locales/fr/dashboard.json');
+      home: typeof import('../../public/locales/fr/home.json');
+    };
+  }
+} 
